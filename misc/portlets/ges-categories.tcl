f97233180c11dd7532c2620e853c547b1b7cb0db
Hector Romojaro <hector.romojaro@gmail.com>
2018-05-09 15:33:30 +0000

    Revert massive replacement of empty list creation sentences. The use of '[list]' instead of '{}' adds semantics that could be used for performance improvements in the future, such as using a different internal representation. There is already work in this direction, avoiding the generation of the string representation during comparison of empty strings (huge thanks to Stefan Sobernig for the pointer: https://core.tcl.tk/tcl/info/44527c632ed609c2).

--- misc/portlets/ges-categories.tcl
+++ misc/portlets/ges-categories.tcl
@@ -34,7 +34,7 @@
     if {!$no_tree_name} {
       append content "<h3>$my_tree_name</h3>"
     }
-    set categories {}
+    set categories [list]
     set pos 0
     set cattree(0) [::xowiki::CatTree new -volatile -orderby pos -name $my_tree_name]
     foreach category_info [category_tree::get_tree $tree_id] {
